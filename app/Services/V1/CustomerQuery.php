994a8301a10dd2dcabad79480d719249efb6ee7e
donalawa <awadonalcien12@gmail.com>
2023-01-01 21:41:25 +0100

    Finished Setting Up Models and Migrations

---
+++ app/Services/V1/CustomerQuery.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace App\Services\V1;
+
+use illuminate\Http\Request;
+
+class CustomerQuery {
+    protected $allowedParams = [
+        'name' => ['eq'],
+        'type' => ['eq'],
+        'email' => ['eq'],
+        'address' => ['eq'],
+        'city' => ['eq'],
+        'state' => ['eq'],
+        'postalCode' => ['gt','eq','lt']
+    ];
+
+    protected $columnMap = [
+        'postalCode' => 'postal_code',
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '≤',
+        'gt' => '>',
+        'gte' => '≥',
+    ];
+
+    public function transform(Request $request) {
+        $eloQuery = [];
+    }
+}
